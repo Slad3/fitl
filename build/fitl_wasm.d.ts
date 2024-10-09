6d108385e3de53997284f9b439b5e29d10e5218d
Ben <feba23@live.com>
2024-10-09 14:42:41 -0500

    Updates

---
+++ build/fitl_wasm.d.ts
@@ -0,0 +1,53 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {any} input_table
+* @returns {any}
+*/
+export function test_json_input(input_table: any): any;
+/**
+* @param {string} input_query
+* @param {any} input_table
+* @param {string} table_format
+* @returns {any}
+*/
+export function fitl_filter(input_query: string, input_table: any, table_format: string): any;
+/**
+*/
+export enum TableFormat {
+  Custom = 0,
+  JsonArray = 1,
+}
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly test_json_input: (a: number, b: number) => void;
+  readonly fitl_filter: (a: number, b: number, c: number, d: number, e: number, f: number) => void;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+  readonly __wbindgen_exn_store: (a: number) => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
